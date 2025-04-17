0858bfa69c770d5e40a14403dbdafd93f34e71b9
Bittu-Kumar089 <bittu6204kk@gmail.com>
2025-04-17 21:56:58 +0530

    Updated commit

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,21 @@ export default defineConfig({
     react(), 
     tailwindcss()
   ],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    strictPort: true
+  }
 })
